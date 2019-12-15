de08268ce3ffb7349ee6d5ffd208b00210187dd6
Bjorn De Craemer <bjorn.decraemer@yahoo.com>
2019-12-15 23:06:13 +0100

    MVP, no refresh

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HeatingService} from "./heater/services/heating.service";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor() {}
+  ngOnInit(): void {
+    this.heatingService.refreshData();
+  }
+  constructor(public heatingService : HeatingService) {
+  }
 
+  public setBurnerStatus(status : boolean){
+    this.heatingService.setBurnerStatus(status);
+  }
+
+  public setPumpStatus(status : boolean){
+    this.heatingService.setPumpStatus(status);
+  }
+
+  public setManualModeStatus(status : boolean){
+    this.heatingService.setManualStatus(status);
+  }
 }
