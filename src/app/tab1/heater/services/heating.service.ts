95a9b8100d9017693b2719aeafd670dafd88c3c7
Bjorn DC <bjorn.decraemer@yahoo.com>
2019-12-19 20:28:05 +0100

    Added error connection toaster

--- src/app/tab1/heater/services/heating.service.ts
+++ src/app/tab1/heater/services/heating.service.ts
@@ -1,46 +1,47 @@
 import {Injectable, OnInit} from '@angular/core';
-import {BehaviorSubject, Subject} from "rxjs";
-import {ApiService} from "./api.service";
+import {BehaviorSubject, Subject} from 'rxjs';
+import {ApiService} from './api.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class HeatingService implements OnInit {
 
-    ngOnInit(): void {
-        this.refreshData();
+    constructor(private apiService: ApiService) {
     }
 
-    public loadingSubject: Subject<Boolean> = new Subject();
+    public loadingSubject: Subject<boolean> = new Subject();
     $burnerStatus: BehaviorSubject<boolean> = new BehaviorSubject(null);
     $pumpStatus: BehaviorSubject<boolean> = new BehaviorSubject(null);
     $temperature: BehaviorSubject<number> = new BehaviorSubject(null);
     $humidity: BehaviorSubject<number> = new BehaviorSubject(null);
     $manualMode: BehaviorSubject<boolean> = new BehaviorSubject(null);
     $requestedTemperature: BehaviorSubject<number> = new BehaviorSubject(null);
+    $connectionError: Subject<string> = new Subject();
     $killSwitch: BehaviorSubject<boolean> = new BehaviorSubject(null);
 
-    private logs: string = "";
-
-    constructor(private apiService: ApiService) {
+    ngOnInit(): void {
+        this.refreshData();
     }
 
     public refreshData() {
         this.loadingSubject.next(true);
-        this.apiService.getPumpStatus().subscribe(value => this.$pumpStatus.next(value));
+        this.apiService.getPumpStatus().subscribe(value => this.$pumpStatus.next(value), (error1) => this.$connectionError.next(error1));
         this.apiService.getBurnerStatus().subscribe(value => this.$burnerStatus.next(value));
         this.apiService.getTemperatureStatus().subscribe(value => this.$temperature.next(value));
         this.apiService.getHumidityStatus().subscribe(value => this.$humidity.next(value));
         this.apiService.getManualModeStatus().subscribe(value => this.$manualMode.next(value));
-        this.apiService.getRequestedTempStatus().subscribe(value => this.$requestedTemperature.next(value));
-        this.loadingSubject.next(false);
+        this.apiService.getRequestedTempStatus().subscribe(value => {
+            this.loadingSubject.next(false);
+            return this.$requestedTemperature.next(value);
+        });
     }
 
     public setBurnerStatus(value: boolean) {
         this.loadingSubject.next(true);
         this.apiService.setBurnerMode(value)
             .subscribe(() => {
-                    this.apiService.getBurnerStatus().subscribe(value => this.$burnerStatus.next(value));
+                    this.apiService.getBurnerStatus().subscribe((burnerStatus) => this.$burnerStatus.next(burnerStatus));
                     this.loadingSubject.next(false);
                 }
             );
@@ -50,7 +51,7 @@ export class HeatingService implements OnInit {
         this.loadingSubject.next(true);
         this.apiService.setPumpMode(value)
             .subscribe(() => {
-                    this.apiService.getPumpStatus().subscribe(value => this.$pumpStatus.next(value))
+                    this.apiService.getPumpStatus().subscribe(pumpStatus => this.$pumpStatus.next(pumpStatus));
                     this.loadingSubject.next(false);
                 }
             );
@@ -60,7 +61,7 @@ export class HeatingService implements OnInit {
         this.loadingSubject.next(true);
         this.apiService.setManualMode(value)
             .subscribe(() => {
-                    this.apiService.getManualModeStatus().subscribe(value => this.$manualMode.next(value))
+                    this.apiService.getManualModeStatus().subscribe(manualModeStatus => this.$manualMode.next(manualModeStatus));
                     this.loadingSubject.next(false);
                 }
             );
@@ -70,7 +71,8 @@ export class HeatingService implements OnInit {
         this.loadingSubject.next(true);
         this.apiService.setRequestedTemperature(value)
             .subscribe(() => {
-                    this.apiService.getRequestedTempStatus().subscribe(value => this.$requestedTemperature.next(value))
+                    this.apiService.getRequestedTempStatus()
+                        .subscribe(requestedTempStatus => this.$requestedTemperature.next(requestedTempStatus));
                     this.loadingSubject.next(false);
                 }
             );
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,22 +1,23 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {HeatingService} from "./heater/services/heating.service";
-import {BehaviorSubject, interval, Observable} from "rxjs";
+import {HeatingService} from './heater/services/heating.service';
+import {BehaviorSubject, interval} from 'rxjs';
+import {ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page implements OnInit, OnDestroy{
+export class Tab1Page implements OnInit, OnDestroy {
 
   knobValues = 0;
   knobLabel = 'Off';
   sliderBadgeColor = 'danger';
-  tempTempVar : number;
+  tempTempVar: number;
   $secondsCounterObservable;
-  $tempReady : BehaviorSubject<boolean> = new BehaviorSubject(false);
+  $tempReady: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
-  constructor(public heatingService : HeatingService) {
+  constructor(public heatingService: HeatingService, public toastController: ToastController) {
   }
 
   ngOnInit(): void {
@@ -28,6 +29,7 @@ export class Tab1Page implements OnInit, OnDestroy{
       this.refreshSlider();
     });
     this.$secondsCounterObservable = interval(15000).subscribe(() => {this.heatingService.refreshData(); });
+    this.heatingService.$connectionError.subscribe(value => this.presentToast(value));
   }
 
   ngOnDestroy(): void {
@@ -35,12 +37,23 @@ export class Tab1Page implements OnInit, OnDestroy{
     this.$tempReady.next(false);
   }
 
+  async presentToast(value: string) {
+      const toast = await this.toastController.create({
+        header: 'Error',
+        message: 'Connecting to the pi failed, please check your wifi settings',
+        position: 'top',
+        duration: 5000,
+        color: 'danger'
+      });
+      toast.present();
 
-  public setBurnerStatus(status : boolean){
+  }
+
+  public setBurnerStatus(status: boolean) {
     this.heatingService.setBurnerStatus(status);
   }
 
-  public setPumpStatus(status : boolean){
+  public setPumpStatus(status: boolean) {
     this.heatingService.setPumpStatus(status);
   }
 
@@ -50,15 +63,15 @@ export class Tab1Page implements OnInit, OnDestroy{
 
   public onSliderChanged() {
     console.log('Current debounced value = ' + this.knobValues);
-    if(!this.tempTempVar || this.tempTempVar !== this.knobValues) {
-      console.log("Persisting!")
+    if (!this.tempTempVar || this.tempTempVar !== this.knobValues) {
+      console.log('Persisting!');
       this.tempTempVar = this.knobValues;
       this.heatingService.setRequestedTemperatureStatus(this.knobValues);
       this.refreshSlider();
     }
   }
 
-  private refreshSlider(){
+  private refreshSlider() {
     if (this.tempTempVar ===  0) {
       this.sliderBadgeColor = 'medium';
       this.knobLabel = 'off';
